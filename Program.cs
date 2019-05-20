b694837fa0971916731482098ab9dc1476aaec04
VGTHuang <1187184299@qq.com>
2019-05-20 16:26:44 +0800

    regex

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ConsoleSandbox
@@ -11,8 +12,28 @@ namespace ConsoleSandbox
         static void Main(string[] args)
         {
             using (Bus myBus = new Bus("asdasd", 12))
+            using (Bus myOtherBus = new Bus("asdasd", 12))
             {
                 myBus.print();
+                if (myBus.Equals(myOtherBus))
+                {
+                    Console.WriteLine("equal");
+                }
+                else
+                {
+                    Console.WriteLine("not equal");
+                }
+            }
+
+
+            string regexTest = "123456abcd789qwer";
+            Regex re = new Regex(@"[0-9]{1,3}[a-z]{1,3}");
+            MatchCollection matches = re.Matches(regexTest);
+            Console.WriteLine("{0} match(es) found in {1}", matches.Count, regexTest);
+
+            foreach(Match m in matches)
+            {
+                Console.WriteLine(m.ToString());
             }
 
             Console.ReadKey();
@@ -32,7 +53,7 @@ namespace ConsoleSandbox
         }
     }
 
-    class Bus : Vehicle, IDisposable
+    class Bus : Vehicle, IDisposable, BusEquatable<Bus>
     {
         private int capacity;
         public Bus(string n, int c) : base(n)
@@ -40,15 +61,26 @@ namespace ConsoleSandbox
             this.capacity = c;
         }
 
-        public void Dispose()
-        {
-            
-        }
+        public void Dispose() { }
 
         public new void print()
         {
             Console.WriteLine("name: " + this.name + "     capacity: " + this.capacity);
         }
+
+        public bool Equals(Bus other)
+        {
+            if(this.capacity == other.capacity && this.name == other.name)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    interface BusEquatable<T>
+    {
+        bool Equals(T obj);
     }
 
 }
