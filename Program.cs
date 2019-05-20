7aa418bfd6854854fcc8f70ac135a77689f19404
VGTHuang <1187184299@qq.com>
2019-05-20 15:37:52 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Bus myBus = new Bus("asdasd", 12))
+            {
+                myBus.print();
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    class Vehicle
+    {
+        protected string name;
+        public Vehicle(string n)
+        {
+            this.name = n;
+        }
+        public void print()
+        {
+            Console.WriteLine("name: " + this.name);
+        }
+    }
+
+    class Bus : Vehicle, IDisposable
+    {
+        private int capacity;
+        public Bus(string n, int c) : base(n)
+        {
+            this.capacity = c;
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public new void print()
+        {
+            Console.WriteLine("name: " + this.name + "     capacity: " + this.capacity);
+        }
+    }
+
+}
